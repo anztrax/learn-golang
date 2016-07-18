32c06bbdaa26c8687679c544016cef3e331e14cf
andrew ananta <andrew@traveloka.com>
2016-07-19 03:40:36 +0700

    done add implementation of method howToUseSelectKeyword

--- main/main.go
+++ main/main.go
@@ -60,7 +60,23 @@ func checkIntefaceOfVariable(){
 }
 
 func howToUseSelectKeyword(){
-  fmt.Println("How to use Select keyword");
+  var c1, c2 , c3 chan int;
+  var i1, i2 int;
+
+  select{
+    case i1 = <-c1:
+      fmt.Printf("received ",i1,"from c1\n");
+    case c2 <- i2:
+      fmt.Printf("sent ",i2, " to c2\n");
+    case i3, ok:= (<-c3):
+      if(ok){
+        fmt.Printf("recieved ",i3," from c3\n");
+      }else{
+        fmt.Printf("c3 is closed\n");
+      }
+    default:
+      fmt.Printf("no communication\n");
+  }
 }
 
 func printAndPlayVariable(){
