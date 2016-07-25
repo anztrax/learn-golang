d12cd227b5fd35f1e444ee8738eea7e0c5a4a90c
andrew ananta <andrew@traveloka.com>
2016-07-25 15:14:02 +0700

    add example of Error, container, sort,and a little bit of read and write file | directory

--- main/main2.go
+++ main/main2.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"container/list"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"sort"
 	"strings"
 )
 
@@ -29,7 +34,7 @@ func trystringsPackage() {
 }
 
 func tryosPackage() {
-	theHardWay := func() {
+	theHardWayToReadFile := func() {
 		file, err := os.Open("test.txt")
 		if err != nil {
 			//handle error here
@@ -54,10 +59,105 @@ func tryosPackage() {
 		str := string(bs)
 		fmt.Println(str)
 	}
-	theHardWay()
+
+	theMoreSimpleWayToReadFile := func() {
+		bs, err := ioutil.ReadFile("test.txt")
+		if err != nil {
+			return
+		}
+
+		str := string(bs)
+		fmt.Println(str)
+	}
+
+	theMoreSimpleWayToWriteFile := func() {
+		file, err := os.Create("test2.txt")
+		if err != nil {
+			return
+		}
+		defer file.Close()
+		file.WriteString("test")
+	}
+
+	readDirectory := func() {
+		dir, err := os.Open(".")
+		if err != nil {
+			return
+		}
+		defer dir.Close()
+
+		fileInfos, err := dir.Readdir(-1)
+		if err != nil {
+			return
+		}
+
+		for _, fi := range fileInfos {
+			fmt.Println("file name : ", fi.Name())
+		}
+	}
+
+	walkTroughTheDirectory := func() {
+		filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+			fmt.Println(path)
+			return nil
+		})
+	}
+
+	theHardWayToReadFile()
+	theMoreSimpleWayToReadFile()
+	theMoreSimpleWayToWriteFile()
+	readDirectory()
+	walkTroughTheDirectory()
+}
+
+func tryError() {
+	err := errors.New("Error Message")
+	fmt.Println("error : ", err)
+}
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+//type of ByName is array of Person ?
+type ByName []Person
+
+//sort is an interface, so they need to implement 3 func to use sort capabilities
+func (this ByName) Len() int {
+	return len(this)
+}
+func (this ByName) Less(i, j int) bool {
+	return this[i].Name < this[j].Name
+}
+func (this ByName) Swap(i, j int) {
+	this[i], this[j] = this[j], this[i]
+}
+
+func tryContainerAndSort() {
+	//list here is linked list
+	var x list.List
+
+	x.PushBack(1)
+	x.PushBack(2)
+	x.PushBack(3)
+
+	for e := x.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value.(int)) //what is is mean ?
+	}
+
+	kids := []Person{
+		{"Amebo", 9},
+		{"Pico", 7},
+	}
+
+	sort.Sort(ByName(kids))
+	fmt.Println(kids)
 }
 
 func main() {
 	trystringsPackage()
 	tryosPackage()
+	tryError()
+	tryContainerAndSort()
 }
